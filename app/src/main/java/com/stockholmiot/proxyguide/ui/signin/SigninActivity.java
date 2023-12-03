130f00a887f236f99dfebc84ee9cde626158ce2e
Bienvenue MBAIWODJI <bienvenuembaiwodji9@gmail.com>
2023-12-03 04:10:52 +0100

    create the login interface

---
+++ app/src/main/java/com/stockholmiot/proxyguide/ui/signin/SigninActivity.java
@@ -0,0 +1,16 @@
+package com.stockholmiot.proxyguide.ui.signin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.stockholmiot.proxyguide.R;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
